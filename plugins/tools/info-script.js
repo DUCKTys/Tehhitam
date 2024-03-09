5773222030cb4e2ab88bdbae30561064920955bb
Tanaka <projectthenext1532@gmail.com>
2024-03-09 07:20:15 +0700

    Update info-script.js

--- plugins/tools/info-script.js
+++ plugins/tools/info-script.js
@@ -3,7 +3,7 @@ export default {
    tags: ['script'],
    command: ['script', 'sc', 'repo', 'repositori'],
    start: async (m) => {
-      let script = 'Menggunakan Base Script Ini \n\nhttps://github.com/ruhend2001/maleficent'
+      let script = 'Menggunakan Base Script Ini \n\nhttps://github.com/Nurutomo/wabot-aq'
       m.adsReply(script, setting.thumbnail, m.chat)
    }
 };
